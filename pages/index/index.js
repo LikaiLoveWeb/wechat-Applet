381d41b631409fc9c9b1882b15837216944cf3ae
likai_360@163.com <likai360>
2018-11-29 09:03:33 +0800

    合租宝 开发首页静态页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,9 +5,52 @@ Page({
    * 页面的初始数据
    */
   data: {
+    scrollTop:0,
+    height_header:25,
+    last_scroll_top:0
+    },
+  scroll(e){
+    console.log(e)
+    let scrollTop = e.detail.scrollTop;
+    // console.log(scrollTop);
+    let height_header = this.data.height_header;
+    let last_scroll_top = this.data.last_scroll_top;
+
+    let len = scrollTop / 10;
+    len = parseInt(len, 10);
+
+    if (scrollTop > last_scroll_top){
+      // 下滑
+      height_header-=len;
+    }else{
+      //上滑
+      height_header += len;
+    }
+
+    if (height_header >= 25){
+      height_header = 25;
+    } else if (height_header <= 15){
+      height_header = 15;
+    }
+
+    console.log(height_header)
+
+    this.setData({
+      height_header,
+      last_scroll_top: scrollTop
+    })
+
+
+    // this.setData({
+    //   scrollTop:0
+    // })
+  },
+  lower(){
 
   },
+  upper(){
 
+  },
   /**
    * 生命周期函数--监听页面加载
    */
